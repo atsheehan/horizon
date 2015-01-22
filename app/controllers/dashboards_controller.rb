79658be9de3a644aabf38af9e609754e7d1704cf
Dan Pickett <dan.pickett@launchware.com>
2015-01-22 17:53:41 -0500

    scope to current_user’s received feed items

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,7 +5,7 @@ class DashboardsController < ApplicationController
     @upcoming_assignments = current_user.assignments.order(due_on: :asc)
     @announcements = current_user.announcements.order(created_at: :desc).limit(5)
     @feed_items = Feedster::DecoratedCollection.new(
-      FeedItem.order('created_at DESC').limit(25)).decorate
+      current_user.received_feed_items.order('created_at DESC').limit(25)).decorate
 
   end
 end
