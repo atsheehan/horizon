7fd77cf8f82f5a6651b80fcf6b5c231ae44ca036
Spencer Dixon <spencercdixon@gmail.com>
2015-02-19 13:06:27 -0500

    Finished writing tests for the votable module

--- spec/lib/votable_spec.rb
+++ spec/lib/votable_spec.rb
@@ -1,19 +1,39 @@
 require 'rails_helper'
 
 describe Votable do
+  let(:question) { FactoryGirl.create(:question) }
+  let(:user) { FactoryGirl.create(:user) }
+
   describe '#total_votes' do
-    it 'sums the total votes'
+
+    it 'sums the total votes' do
+      Vote.create(votable_id: question.id,
+                  votable_type: "Question",
+                  user_id: user.id,
+                  score: 1)
+      expect(question.total_votes).to eq 1
+    end
   end
 
   describe '#increment_vote' do
-    it 'adds 1 to score'
+    it 'adds 1 to score' do
+      question.increment_vote(user)
+      expect(question.votes.first.score).to eq 1
+    end
   end
 
   describe '#decrement_vote' do
-    it 'decreases 1 from score'
+    it 'decreases 1 from score' do
+      question.increment_vote(user)
+      expect(question.votes.first.score).to eq 1
+
+      question.decrement_vote(user)
+      question.decrement_vote(user)
+      expect(question.votes.first.score).to eq -1
+    end
   end
 
-  describe '#question' do
+  describe '#vote_question' do
     let(:question) { FactoryGirl.create(:question) }
 
     context 'given a question' do
