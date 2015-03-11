81c161878340f120bcac5960d7722e577ac594c3
cmkoller <cmkoller01@gmail.com>
2015-03-11 17:05:45 -0400

    Refactor SearchResult object to use for searching lessons and questions [#90135484]

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,35 +1,10 @@
 class SearchesController < ApplicationController
   def index
-    @active_type = params[:type]
-    @lessons = filter_lessons(current_user, params[:query])
-    @questions = filter_questions(params[:query])
-    @results = (@lessons + @questions).length
-  end
+    search_result = SearchResult.new(params[:query], params[:type], current_user)
 
-  private
-
-  def filter_lessons(user, query)
-    lessons = Lesson.search(query)
-    lessons = type_filter(params[:type], lessons)
-    user ? lessons.visible_for(user) : lessons.public
-  end
-
-  def type_filter(type, lessons)
-    if type
-      type != "question" ? lessons.type(type) : Lesson.none
-    else
-      lessons
-    end
-  end
-
-  def filter_questions(query)
-    results = []
-    Question.search(query).each { |question| results << question }
-    Answer.search(query).each { |answer| results << answer.question }
-    if params[:type]
-      params[:type] == "question" ? results : []
-    else
-      results
-    end
+    @active_type = search_result.type
+    @lessons = search_result.lessons
+    @questions = search_result.questions
+    @results = search_result.total
   end
 end
