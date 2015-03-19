b388a651e53276326347e758125116c3b09e91bf
cmkoller <cmkoller01@gmail.com>
2015-03-19 14:30:59 -0400

    Order filtered lessons by recently created

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -181,6 +181,10 @@ FactoryGirl.define do
     body "This is definitely a question."
     category "Other"
     user
+
+    factory :code_syntax_question do
+      category "Code syntax"
+    end
   end
 
   factory :question_queue do
